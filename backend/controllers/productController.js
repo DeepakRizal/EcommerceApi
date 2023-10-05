5c2ce34b83cffa19124dd3c6e82e360505c90926
Deepak Rizal <deepak2000rizal@gmail.com>
2023-10-05 07:09:58 +0530

    Initial commit

---
+++ backend/controllers/productController.js
@@ -0,0 +1,173 @@
+const ErrorHandler = require("../utils/errorHandler");
+const Product = require("./../modals/productModal");
+const catchAsyncErrors = require("./../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
+
+//Create product -- Admin
+exports.createProduct = catchAsyncErrors(async (req, res, next) => {
+  req.body.user = req.user.id;
+
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    success: true,
+    product,
+  });
+});
+
+//Get all product
+exports.getAllProducts = catchAsyncErrors(async (req, res) => {
+  const resultPerPage = 5;
+  const productCount = await Product.countDocuments();
+
+  const apiFeature = new ApiFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
+  const products = await apiFeature.query;
+
+  res.status(200).json({
+    success: true,
+    products,
+    productCount,
+  });
+});
+
+//get Product Details
+exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
+
+//update product-- admin
+exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: true,
+  });
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
+//Delete product
+exports.deleteProduct = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new ErrorHandler("Product not found", 404));
+  }
+  await product.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    message: "product deleted successfully",
+  });
+});
+
+//create New Review or Update the review
+exports.createProductReview = catchAsyncErrors(async (req, res, next) => {
+  const { rating, comment, productId } = req.body;
+
+  const review = {
+    user: req.user._id,
+    name: req.user.name,
+    rating: Number(rating),
+    comment,
+  };
+
+  const product = await Product.findById(productId);
+
+  const isReviewed = product.reviews.find(
+    (rev) => rev.user.toString() === req.user._id.toString()
+  );
+
+  if (isReviewed) {
+    product.reviews.forEach((rev) => {
+      if (rev.user.toHexString() === req.user._id.toString()) {
+        (rev.rating = rating), (rev.comment = comment);
+      }
+    });
+  } else {
+    product.reviews.push(review);
+    product.numOfReviews = product.reviews.length;
+  }
+
+  let avg = 0;
+  product.ratings = product.reviews.forEach((rev) => {
+    avg += rev.rating;
+  });
+
+  product.ratings = avg / product.reviews.length;
+
+  await product.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+//get all Reviews of a product
+exports.getProductReviews = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.query.id);
+
+  if (!Product) {
+    return next(new ErrorHandler("product not found", 400));
+  }
+
+  res.status(200).json({
+    success: true,
+    reviews: product.reviews,
+  });
+});
+
+//Delete Review
+exports.deleteReview = catchAsyncErrors(async (req, res, next) => {
+  const product = await Product.findById(req.query.productId);
+
+  if (!product) {
+    return next(new ErrorHandler("product not found", 400));
+  }
+
+  const reviews = product.reviews.filter(
+    (rev) => rev._id.toString() !== req.query.id.toString()
+  );
+
+  let avg = 0;
+  reviews.forEach((rev) => {
+    avg += rev.rating;
+  });
+
+  const ratings = avg / reviews.length;
+
+  const numOfReviews = reviews.length;
+
+  await Product.findByIdAndUpdate(
+    req.query.productId,
+    {
+      reviews,
+      ratings,
+      numOfReviews,
+    },
+    {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    }
+  );
+
+  res.status(200).json({
+    success: true,
+  });
+});
---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const app = require("./app");
+const dotenv = require("dotenv");
+const connectDatabase = require("./config/database");
+
+//handling Uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error:${err.message}`);
+  console.log(`Shutting down the server due to uncaughtException`);
+  process.exit(1);
+});
+
+//config
+dotenv.config({ path: "backend/config/config.env" });
+
+//connectiong to database
+connectDatabase();
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(`server is working on http://localhost:${process.env.PORT}`);
+});
+
+//Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error:${err.message}`);
+  console.log(`Shutting down the server due to unhandled promise Rejection`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
